ed2780a63760c7012f8f6b4eae7ee0bdb8db18ab
Hansaja Sooriyapperuma <hansajaps@gmail.com>
2026-03-29 15:45:37 +0530

    Add files via upload

--- src/main/java/com/example/marketplace/MarketplaceApplication.java
+++ src/main/java/com/example/marketplace/MarketplaceApplication.java
@@ -1,17 +1,12 @@
 package com.example.marketplace;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MarketplaceApplication {
     public static void main(String[] args) {
-        // Load .env file if it exists
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        
         SpringApplication.run(MarketplaceApplication.class, args);
     }
 }
-
 
